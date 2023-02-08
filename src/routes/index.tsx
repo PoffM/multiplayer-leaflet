6cd03ddc796ef18de842598fce1983de3a454c13
Mathew Poff <matp@live.ca>
2023-02-08 10:24:11 -0500

    Got rid of the unnecessary start screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,54 +1,45 @@
 import { nanoid } from "nanoid";
-import { createSignal } from "solid-js";
-import { useNavigate } from "solid-start";
+import { createSignal, onCleanup, onMount, Show } from "solid-js";
+import { useLocation, useNavigate } from "solid-start";
+import { CopyButton } from "~/components/CopyButton";
+import { MultiplayerLeaflet } from "~/components/MultiplayerLeaflet";
 
 export default function Home() {
+  const location = useLocation();
   const navigate = useNavigate();
 
-  const [roomName, setRoomName] = createSignal("");
+  const [shareLink, setShareLink] = createSignal("");
 
-  function goToRoom(id: string = nanoid()) {
-    navigate(`/room?id=${id}`, {});
-  }
+  onMount(() => {
+    // Create a new random room ID if there isn't one:
+    if (!location.query.roomId) {
+      navigate(`/?roomId=${nanoid()}`);
+    }
+
+    function updateLink() {
+      setTimeout(() => setShareLink(window.location.href), 0);
+    }
+    updateLink();
+
+    window.addEventListener("popstate", updateLink);
+    onCleanup(() => window.removeEventListener("popstate", updateLink));
+  });
 
   return (
     <div class="fixed inset-0 flex items-center justify-center">
-      <main class="w-[400px] space-y-4">
+      <main class="space-y-4">
         <h1 class="font-extrabold text-4xl text-center">Multiplayer Leaflet</h1>
-        <div class="card p-6 bg-base-200 space-y-4">
-          <div class="flex justify-center">
-            <button class="btn btn-primary" onClick={() => goToRoom()}>
-              Create a New Room
-            </button>
-          </div>
-          <div class="font-bold text-center">OR</div>
-          <form
-            class="flex justify-center"
-            onSubmit={(e) => {
-              e.preventDefault();
-              goToRoom(roomName());
-            }}
-          >
-            <div class="form-control">
-              <div class="input-group">
-                <input
-                  type="text"
-                  class="input input-bordered"
-                  placeholder="Enter a Room Name"
-                  value={roomName()}
-                  // @ts-expect-error "value" isn't on target for some reason, should work anyway:
-                  onInput={(e) => setRoomName(e.target.value)}
-                />
-                <button
-                  type="submit"
-                  class="btn btn-secondary"
-                  disabled={!roomName()}
-                >
-                  Join
-                </button>
-              </div>
+        <Show when={shareLink()}>
+          <div class="flex flex-col items-center gap-1">
+            <div>Share this link with someone to share a synchronized map:</div>
+            <div class="input-group flex gap-2 items-center justify-center w-auto border rounded-lg pl-2 border-gray-500">
+              <div class="">{shareLink()}</div>
+              <CopyButton textToCopy={shareLink} />
             </div>
-          </form>
+          </div>
+        </Show>
+        <div class="w-[700px] h-[700px]">
+          <MultiplayerLeaflet roomName={() => location.query.id} />
         </div>
       </main>
     </div>
