18112e129c6847ea7b7fb8fbc08334f68644fbd4
Mathew Poff <matp@live.ca>
2023-02-21 19:27:31 -0500

    Fixed bug where map repositions itself when someone connects

--- src/components/shared-leaflet/syncMapView.tsx
+++ src/components/shared-leaflet/syncMapView.tsx
@@ -18,7 +18,6 @@ export function syncMapView(map: LeafletMap, yState: Y.Map<any>) {
       yState.set("position", newPosition);
     }
   }
-  updateYState();
 
   function handleMove(e: LeafletEvent) {
     /** True when the local user is dragging the map. */
